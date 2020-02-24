cd6877b4bf0f763a1469b93950b6c94127871f22
fabioo2 <fabio.kim2@gmail.com>
2020-02-24 14:41:25 -0500

    first commit

---
+++ PrimeNumberGenerator/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimeNumberGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm = new VM();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void CalculatePrimesButton_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Calc();
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key < Key.D0 || e.Key > Key.D9) && (e.Key < Key.NumPad0 || e.Key > Key.NumPad9) && e.Key != Key.Back && e.Key != Key.Delete && e.Key != Key.Tab)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ PrimeNumberGenerator/VM.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PrimeNumberGenerator
+{
+    class VM : INotifyPropertyChanged
+    {
+        const int INITIAL_INTEGER = 100;
+        public ObservableCollection<string> Output { get; set; } = new ObservableCollection<string>();
+
+        private int enteredNumber = INITIAL_INTEGER;
+        public int EnteredNumber
+        {
+            get { return enteredNumber; }
+            set { enteredNumber = value; NotifyChange(); }
+        }
+
+        public void Calc()
+        {
+            Output.Clear();
+
+            for (int i = 1; i <= EnteredNumber; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Output.Add(i.ToString());
+                }
+            }
+        }
+        public bool IsPrime(int number)
+        {
+            for (int j = 2; j < number; j++)
+            {
+                if (number % j == 0)
+                    return false;
+            }
+            return true;
+        }
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyChange([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        #endregion 
+    }
+}
